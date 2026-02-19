a1a69c18f7dd68cebf94096e2da42d9871d53606
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2026-02-19 19:57:58 +0600

    feat: add rubik and open sans fornt

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,18 @@
 import type { Metadata } from "next";
+import { Open_Sans, Rubik } from "next/font/google";
 import "./globals.css";
+
+const rubik = Rubik({
+  subsets: ["latin"],
+  variable: "--font-rubik",
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
+});
+
+const openSans = Open_Sans({
+  subsets: ["latin"],
+  variable: "--font-open-sans",
+  weight: ["300", "400", "500", "600", "700", "800"],
+});
 
 export const metadata: Metadata = {
   title: "Kicks",
@@ -13,7 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="antialiased">{children}</body>
+      <body className={`${rubik.variable} ${openSans.variable} antialiased font-rubik`}>
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1>Kicks</h1>;
+  return <h1 className="font-rubik">Kicks</h1>;
 }
