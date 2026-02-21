db44d32c081b48b4bdb3f6ac6d9d1e8555761687
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2026-02-21 09:55:16 +0600

    feat: add responsiive styles for Hero section

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,11 +1,11 @@
+import { heroThumbnails } from "@/constants";
 import heroImage from "@/public/assets/home/hero/hero-image-1.png";
 import Image from "next/image";
 import Button from "../common/Button";
-import { heroThumbnails } from "@/constants";
 
 export default function Hero() {
   return (
-    <section className="mt-8">
+    <section className="mt-6 lg:mt-8">
       {/* Background Headline */}
       <h2 className="text-[60px] sm:text-[80px] md:text-[120px] lg:text-[170px] xl:text-[223px] [@media(max-width:1330px)]:xl:text-[210px] text-center font-bold leading-[100%] tracking-normal uppercase text-dark-gray">
         DO IT <span className="text-primary">RIGHT</span>
@@ -18,35 +18,39 @@ export default function Hero() {
           src={heroImage}
           alt="Nike Air Max Hero"
           fill
-          className="object-cover transition-transform duration-700 group-hover:scale-105"
+          className="object-cover w-full h-full bg-center bg-cover min-h-89.5 transition-transform duration-700 group-hover:scale-105"
           priority
         />
 
         {/* Vertical Side Badge - Left */}
-        <div className="absolute left-0 top-20 ">
-          <div className="bg-dark-gray p-6 rounded-r-2xl flex items-center justify-center">
-            <span className="[writing-mode:vertical-lr] rotate-180 text-[16px] text-gray font-semibold tracking-normal">
+        <div className="absolute left-0 top-5.75 lg:top-20">
+          <div className="bg-dark-gray p-2 lg:p-6 rounded-r-lg lg:rounded-r-2xl flex items-center justify-center">
+            <span className="[writing-mode:vertical-lr] rotate-180 text-[12px] lg:text-[16px] text-gray font-semibold tracking-normal">
               Nike product of the year
             </span>
           </div>
         </div>
 
         {/* Content Corner - Bottom Left */}
-        <div className="absolute left-8 lg:left-12 bottom-8 lg:bottom-12 flex flex-col gap-4 lg:gap-6 z-10">
+        <div className="absolute left-4 lg:left-12 bottom-4 lg:bottom-12 flex flex-col gap-2 lg:gap-6 z-10">
           <div className="flex flex-col gap-2">
-            <h1 className="text-white text-[24px] lg:text-[74px] leading-[100%] font-semibold">
-              Nike Air Max
-            </h1>
-            <p className="font-open-sans text-gray text-[24px] max-w-122.5 font-semibold">
-              Nike introducing the new air max for everyone's comfort
-            </p>
+            <div className="flex flex-col gap-0 lg:gap-2">
+              <h1 className="text-white text-[24px] lg:text-[74px] leading-[100%] font-semibold">
+                Nike Air Max
+              </h1>
+              <p className="font-open-sans text-gray text-[14px] lg:text-[24px] max-w-49.25 lg:max-w-122.5 font-semibold">
+                Nike introducing the new air max for everyone's comfort
+              </p>
+            </div>
           </div>
 
-          <Button variant="primary">SHOP NOW</Button>
+          <Button variant="primary" className="py-[7.5px]">
+            SHOP NOW
+          </Button>
         </div>
 
         {/* Gallery Thumbnails - Right */}
-        <div className="absolute right-8 bottom-8 hidden sm:flex flex-col gap-4">
+        <div className="absolute right-4 lg:right-8 bottom-4 lg:bottom-8 flex flex-col gap-2 lg:gap-4">
           {heroThumbnails.map((item) => (
             <div
               key={item.id}
