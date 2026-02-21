4638c099bf9196beb36303b57867392e8f401e6b
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2026-02-21 09:13:32 +0600

    chore: fix spelling mistates of image imports

--- constants.ts
+++ constants.ts
@@ -1,10 +1,10 @@
-import menuDropDown from "@/public/assets/icons/navbar/caret_down.png";
 import thumbnail1 from "@/public/assets/home/hero/hero-image-2.png";
 import thumbnail2 from "@/public/assets/home/hero/hero-image-3.png";
-import produtImage1 from "@/public/assets/home/new-drops/product-1.png";
-import produtImage2 from "@/public/assets/home/new-drops/product-2.png";
-import produtImage3 from "@/public/assets/home/new-drops/product-3.png";
-import produtImage4 from "@/public/assets/home/new-drops/product-4.png";
+import productImage1 from "@/public/assets/home/new-drops/product-1.png";
+import productImage2 from "@/public/assets/home/new-drops/product-2.png";
+import productImage3 from "@/public/assets/home/new-drops/product-3.png";
+import productImage4 from "@/public/assets/home/new-drops/product-4.png";
+import menuDropDown from "@/public/assets/icons/navbar/caret_down.png";
 
 export const navMenu = [
   {
@@ -38,25 +38,25 @@ export const heroThumbnails = [
 export const newDropsProducts = [
   {
     id: 1,
-    image: produtImage1,
+    image: productImage1,
     title: "ADIDAS 4DFWD X PARLEY RUNNING SHOES",
     price: 125,
   },
   {
     id: 2,
-    image: produtImage2,
+    image: productImage2,
     title: "ADIDAS 4DFWD X PARLEY RUNNING SHOES",
     price: 125,
   },
   {
     id: 3,
-    image: produtImage3,
+    image: productImage3,
     title: "ADIDAS 4DFWD X PARLEY RUNNING SHOES",
     price: 125,
   },
   {
     id: 4,
-    image: produtImage4,
+    image: productImage4,
     title: "ADIDAS 4DFWD X PARLEY RUNNING SHOES",
     price: 125,
   },
