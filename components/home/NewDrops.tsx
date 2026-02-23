53d7ab520b6af86cab5a05650014de51821d12f6
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2026-02-23 20:02:45 +0600

    chore: remove logs

--- components/home/NewDrops.tsx
+++ components/home/NewDrops.tsx
@@ -6,7 +6,6 @@ import { getProducts } from "@/data/products";
 
 export default async function NewDrops() {
   const products = await getProducts();
-  console.log(products.length);
 
   return (
     <div className="body-width px-4 lg:px-0 pt-6 lg:pt-22.5 pb-4 lg:pb-32 flex flex-col gap-4 lg:gap-8">
