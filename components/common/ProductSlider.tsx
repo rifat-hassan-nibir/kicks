749b442591fe155978e3da06a6dd269aa8544c8c
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2026-02-23 09:36:20 +0600

    feat: add ProductSlider component and update the SliderButton compnent to render different types of buttons depending on prop value

---
+++ components/common/ProductSlider.tsx
@@ -0,0 +1,54 @@
+import { StaticImageData } from "next/image";
+import ProductCard from "./ProductCard";
+import SectionTitle from "./SectionTitle";
+import SliderButton from "./SliderButton";
+
+interface Product {
+  id: string | number;
+  image: string | StaticImageData | any;
+  title: string;
+  price: number | string;
+}
+
+interface ProductSliderProps {
+  title: React.ReactNode;
+  products: Product[];
+  actionButton?: React.ReactNode;
+  showSliderButtons?: boolean;
+}
+
+export default function ProductSlider({
+  title,
+  products,
+  actionButton,
+  showSliderButtons = true,
+}: ProductSliderProps) {
+  return (
+    <div>
+      <div className="flex items-end justify-between mb-4 lg:mb-8 gap-4">
+        <h4 className="font-rubik text-dark-gray text-[24px] lg:text-[48px] font-semibold leading-[100%]">
+          {title}
+        </h4>
+        <div className="flex items-center gap-4">
+          {actionButton}
+          {showSliderButtons && (
+            <div className="flex gap-2">
+              <SliderButton direction="left" type="dark-gray" className="bg-dark-gray/40" />
+              <SliderButton direction="right" type="dark-gray" />
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-4 gap-y-6">
+        {products.map((item) => (
+          <ProductCard
+            key={item.id}
+            productImage={item.image}
+            title={item.title}
+            price={item.price}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
--- components/common/SliderButton.tsx
+++ components/common/SliderButton.tsx
@@ -1,36 +1,40 @@
 import { cn } from "@/lib/utils";
-import arrowLeft from "@/public/assets/icons/arrow-left.png";
-import arrowRight from "@/public/assets/icons/arrow-right.png";
+import arrowLeftBlack from "@/public/assets/icons/arrow-left-black.svg";
+import arrowLeftWhite from "@/public/assets/icons/arrow-left-white.svg";
+import arrowRightBlack from "@/public/assets/icons/arrow-right-black.svg";
+import arrowRightWhite from "@/public/assets/icons/arrow-right-white.svg";
 import Image from "next/image";
 
 export default function SliderButton({
   direction,
   className,
+  type,
 }: {
   direction: "left" | "right";
   className?: string;
+  type?: "gray" | "dark-gray";
 }) {
   return (
     <button
       className={cn(
-        "w-8 lg:w-10 h-8 lg:h-10 rounded-lg hover:bg-white transition-colors border-none bg-gray flex items-center justify-center duration-300 group hover:cursor-pointer active:scale-90",
+        `w-8 lg:w-10 h-8 lg:h-10 rounded-lg hover:bg-white transition-colors border-none ${type === "gray" ? "bg-gray" : type === "dark-gray" ? "bg-dark-gray" : "bg-gray"} flex items-center justify-center duration-300 group hover:cursor-pointer active:scale-90`,
         className,
       )}
     >
       {direction === "left" ? (
         <Image
-          src={arrowLeft}
+          src={type === "gray" ? arrowLeftBlack : arrowLeftWhite}
           alt="Arrow Left"
-          width={4.5}
-          height={9}
+          width={16}
+          height={16}
           className="group-hover:invert transition-all"
         />
       ) : (
         <Image
-          src={arrowRight}
+          src={type === "gray" ? arrowRightBlack : arrowRightWhite}
           alt="Arrow Right"
-          width={4.5}
-          height={9}
+          width={16}
+          height={16}
           className="group-hover:invert transition-all"
         />
       )}
