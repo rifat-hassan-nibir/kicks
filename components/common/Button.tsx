beb15ac8409c1edc36b7068c038e614b69834fff
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2026-02-23 22:10:15 +0600

    feat: add disabled prop buttons

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -5,18 +5,24 @@ export default function Button({
   className,
   variant,
   onClick,
+  disabled,
 }: {
   children: React.ReactNode;
   className?: string;
   variant?: "primary" | "secondary";
   onClick?: () => void;
+  disabled?: boolean;
 }) {
   return (
     <button
       onClick={onClick}
+      disabled={disabled}
       className={cn(
-        "hover:bg-dark-gray hover:cursor-pointer text-white text-[14px] leading-[100%] tracking-[0.25px] font-medium w-fit p-4 lg:px-8 py-[11.5px] lg:py-[15.5px] rounded-lg transition-all transform active:scale-95",
+        "text-white text-[14px] leading-[100%] tracking-[0.25px] font-medium w-fit p-4 lg:px-8 py-[11.5px] lg:py-[15.5px] rounded-lg transition-all",
         variant === "primary" ? "bg-primary" : "bg-dark-gray",
+        disabled
+          ? "opacity-50 cursor-not-allowed"
+          : "hover:bg-dark-gray hover:cursor-pointer transform active:scale-95",
         className,
       )}
     >
