80177e134ebff79af034af29823326260ea8b01e
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2026-02-25 12:33:36 +0600

    feat: implement no caching strategy and handle error

--- data/products.ts
+++ data/products.ts
@@ -1,25 +1,41 @@
 export const getProducts = async () => {
-  const res = await fetch("https://api.escuelajs.co/api/v1/products");
+  const res = await fetch("https://api.escuelajs.co/api/v1/products", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) throw new Error("Failed to fetch products");
+
   const data = await res.json();
-  return data.slice(12, 20);
+  return data;
 };
 
 export const getProductById = async (id: string) => {
-  const res = await fetch(`https://api.escuelajs.co/api/v1/products/${id}`);
-  if (!res.ok) return null;
+  const res = await fetch(`https://api.escuelajs.co/api/v1/products/${id}`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) throw new Error("Failed to fetch product");
+
   const data = await res.json();
   return data;
 };
 
 export const getRelatedProductsById = async (id: string) => {
-  const res = await fetch(`https://api.escuelajs.co/api/v1/products/${id}/related`);
-  if (!res.ok) return null;
+  const res = await fetch(`https://api.escuelajs.co/api/v1/products/${id}/related`, {
+    cache: "no-store",
+  });
+  if (!res.ok) throw new Error("Failed to fetch related products");
   const data = await res.json();
-  return data.slice(2, 6);
+  return data.slice(0, 4);
 };
 
 export const getCategories = async () => {
-  const res = await fetch("https://api.escuelajs.co/api/v1/categories");
+  const res = await fetch("https://api.escuelajs.co/api/v1/categories", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) throw new Error("Failed to fetch categories");
+
   const data = await res.json();
   return data.slice(0, 2);
 };
